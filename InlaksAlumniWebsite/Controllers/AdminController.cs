f6b78e590253b32def0a7de32a0b3772c335fd47
Ogiogio23 <emmanuel.ogiogio@gmail.com>
2020-08-22 14:21:03 +0100

    Initial commit

---
+++ InlaksAlumniWebsite/Controllers/AdminController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InlaksAlumniWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Title = "Dashboard";
+            return View();
+
+        }
+
+        public ActionResult CreateAdmin()
+        {
+            ViewBag.Title = "CreateAdmin";
+            return View();
+        }
+
+        public ActionResult CreateAlumni()
+        {
+            ViewBag.Title = "CreateAlumni";
+            return View();
+        }
+
+        public ActionResult CreateDonation()
+        {
+            ViewBag.Title = "CreateDonation";
+            return View();
+        }
+
+        public ActionResult CreateEvent()
+        {
+            ViewBag.Title = "CreateEvent";
+            return View();
+        }
+
+        public ActionResult Inbox()
+        {
+            ViewBag.Title = "Inbox";
+            return View();
+        }
+
+        public ActionResult UpdateAdmin()
+        {
+            ViewBag.Title = "UpdateAdmin";
+            return View();
+        }
+
+        public ActionResult UpdateAlumni()
+        {
+            ViewBag.Title = "UpdateAlumni";
+            return View();
+        }
+
+        public ActionResult UpdateDonation()
+        {
+            ViewBag.Title = "UpdateDonation";
+            return View();
+        }
+
+        public ActionResult ManageEvent()
+        {
+            ViewBag.Title = "UpdateEvent";
+            return View();
+        }
+
+        public ActionResult UploadPhoto()
+        {
+            ViewBag.Title = "UploadPhoto";
+            return View();
+        }
+
+        public ActionResult ViewCollections()
+        {
+            ViewBag.Title = "ViewCollections";
+            return View();
+        }
+
+        public ActionResult ViewGallery()
+        {
+            ViewBag.Title = "ViewGallery";
+            return View();
+        }
+    }
+}
---
+++ InlaksAlumniWebsite/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InlaksAlumniWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About Us.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            ViewBag.Title = "Event";
+
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            ViewBag.Title = "Gallery.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Title = "Login.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Title = "New Alumni.";
+
+            return View();
+        }
+
+        public ActionResult Payment()
+        {
+            ViewBag.Title = "New Alumni.";
+
+            return View();
+        }
+
+        public ActionResult AlumniProfile()
+        {
+            ViewBag.Title = "New Alumni.";
+
+            return View();
+        }
+
+        public ActionResult UpdateAccount()
+        {
+            ViewBag.Title = "New Alumni.";
+
+            return View();
+        }
+
+        public ActionResult UploadEvidence()
+        {
+            ViewBag.Title = "New Alumni.";
+
+            return View();
+        }
+    }
+}
